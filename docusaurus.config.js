286c585c9c1c83b86f4a421c68ab0cc7918d8328
hyc3z <tubao9hao@126.com>
2022-12-31 20:31:50 +0800

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -19,7 +21,6 @@ const config = {
   scripts: [
     '/js/midiplayer.js',
   ],
-
   presets: [
     [
       'classic',
@@ -28,6 +29,10 @@ const config = {
         docs: {
           routeBasePath: '/docs',
           sidebarPath: require.resolve('./sidebars.js'),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+          sidebarCollapsible: false,
+          sidebarCollapsed: false
         },
         blog: {
           showReadingTime: true,
@@ -54,8 +59,8 @@ const config = {
             docId: 'intro',
             position: 'left',
             label: 'Notes',
+            collapsed: false,
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/hyc3z',
             label: 'Hyc3z',
@@ -68,6 +73,15 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 };
 
 module.exports = config;
