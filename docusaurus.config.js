13e57a8dbf1c7b6f9f9289a9485991be6807de70
hyc3z <tubao9hao@126.com>
2023-01-01 12:36:37 +0800

    Update to nightly

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,85 +4,84 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const math = require('remark-math');
-const katex = require('rehype-katex');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: `Hyc3z's blog`,
-  tagline: 'Effortlessness is a myth.',
-  url: 'https://hyc3z.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'hyc3z', // Usually your GitHub org/user name.
-  projectName: 'hyc3z.github.io', // Usually your repo name.
-  staticDirectories: ['static'],
-  scripts: [
-    '/js/midiplayer.js',
-  ],
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+async function createConfig() {
+  const katex = (await import('rehype-katex')).default;
+  return {
+    title: `Hyc3z's blog`,
+    tagline: 'Effortlessness is a myth.',
+    url: 'https://hyc3z.github.io',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'hyc3z', // Usually your GitHub org/user name.
+    projectName: 'hyc3z.github.io', // Usually your repo name.
+    staticDirectories: ['static'],
+    scripts: [
+      '/js/midiplayer.js',
+    ],
+    presets: [
+      [
+        'classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            routeBasePath: '/docs',
+            sidebarPath: require.resolve('./sidebars.js'),
+            remarkPlugins: [math],
+            rehypePlugins: [katex],
+            sidebarCollapsible: false,
+            sidebarCollapsed: false
+          },
+          blog: {
+            showReadingTime: true,
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          routeBasePath: '/docs',
-          sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-          sidebarCollapsible: false,
-          sidebarCollapsed: false
+        navbar: {
+          title: `Hyc3z's blog`,
+          logo: {
+            alt: 'My Site Logo',
+            src: 'img/logo.svg',
+          },
+          items: [
+            {
+              type: 'doc',
+              docId: 'intro',
+              position: 'left',
+              label: 'Notes',
+              collapsed: false,
+            },
+            {
+              href: 'https://github.com/hyc3z',
+              label: 'Hyc3z',
+              position: 'right',
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
       }),
+    stylesheets: [
+      {
+        href: '/katex/katex.min.css',
+        type: 'text/css',
+        crossorigin: 'anonymous',
+      },
     ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: `Hyc3z's blog`,
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Notes',
-            collapsed: false,
-          },
-          {
-            href: 'https://github.com/hyc3z',
-            label: 'Hyc3z',
-            position: 'right',
-          },
-        ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
+  }
 };
 
-module.exports = config;
+module.exports = createConfig;
 
